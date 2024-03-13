cf97a3ace9e3980d8450dea5c1499a7d22bf6248
kimyoungGF <9005253@gmail.com>
2024-03-13 14:32:47 +0900

    testmake

---
+++ c/make/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "a.h"
+#include "b.h"
+
+extern void fun1() {
+    printf("Test2\n");
+}
---
+++ c/make/test3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "b.h"
+#include "c.h"
+
+extern void fun2() {;
+    printf("Test3\n");
+}
