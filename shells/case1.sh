2b6c0d7b2fb99599e0356b55cc783dae520f0271
kimyoungGF <9005253@gmail.com>
2024-03-15 12:24:12 +0900

    Shell Script

---
+++ shells/case1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+a=0
+b=0
+echo -n "Input : " 
+read a
+
+let a = a/10
+
+case "$a" in
+	10)
+	echo A 
+	9)
+	echo A 
+
+	8)
+	echo A 
+	7)
+	echo A 
+	6)
+	echo A 
+B
+
+	
+A
+
+o
+A
+A
+
+if [ $a -ge 90 ]; then
+	echo A
+elif [ $a -ge 80 ]; then
+	echo B
+elif [ $a -ge 70 ]; then
+	echo C
+elif [ $a -ge 60 ]; then
+	echo D
+else
+	echo F
+fi
+
+echo "Thank you ~ Bye!!"
+
