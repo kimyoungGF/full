047e77180f804c703b7a99e90c033f478c1bf496
kimyoungGF <9005253@gmail.com>
2024-03-18 16:28:50 +0900

    shell_new

--- shells/string4.sh
+++ shells/string4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-str="Hello, World, Linux!"
+str="Hello, World, LINUX!"
 echo "${str:(-7):4}"
 
--- shells/string5.sh
+++ shells/string5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-str="Hello, World, Linux!"
-echo $str | cut -c 8-12 
+str="Hello, World, LINUX!"
+echo $str | cut -c 8-12
 
