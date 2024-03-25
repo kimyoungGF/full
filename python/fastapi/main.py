65202ef818a6f527e6ad023a2ceca689420e4b84
kimyoungGF <9005253@gmail.com>
2024-03-25 17:20:02 +0900

    atlas_0325_1

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 def read_root():
     return {"Hello":"World"}
 
-if __name__=='__main__':
-    uvicorn.run(app, host="0.0.0.0",port=3000)
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=3000)
